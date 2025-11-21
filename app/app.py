b7e5a4f39758c3e44fc743fef0eced15f25ce80c
yuvasakthi S <155764035+Sakthiyuva@users.noreply.github.com>
2025-11-21 15:55:59 +0530

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest
+
+app = Flask(__name__)
+
+# Custom metric
+REQUEST_COUNT = Counter('app_requests_total', 'Total number of requests')
+
+@app.route('/')
+def home():
+    REQUEST_COUNT.inc()
+    return "Hello, Monitoring Stack!"
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype='text/plain')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
